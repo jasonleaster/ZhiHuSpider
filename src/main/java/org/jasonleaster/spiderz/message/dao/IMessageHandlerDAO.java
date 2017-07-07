0ffc351817206a3047bba4e0c995894686ea810d
jasonleaster <jasonleaster@gmail.com>
2017-07-07 23:34:09 +0800

    message dao

--- src/main/java/org/jasonleaster/spiderz/message/dao/IMessageHandlerDAO.java
+++ src/main/java/org/jasonleaster/spiderz/message/dao/IMessageHandlerDAO.java
@@ -1,6 +1,7 @@
 package org.jasonleaster.spiderz.message.dao;
 
 import java.util.List;
+import org.apache.ibatis.annotations.Param;
 import org.jasonleaster.spiderz.message.model.MessageModel;
 import org.springframework.stereotype.Repository;
 
@@ -16,24 +17,24 @@ public interface IMessageHandlerDAO {
     /**
      * 创建一个数据库表，用于为消息队列 #queueName 存放消息
      */
-    int createMessageQueue(String queueName);
+    boolean createMessageQueue(String queueName);
 
     /**
      * 插入消息
      */
-    int insertMessages(String queueName, List<String> messages);
+    int insertMessages(String queueName, @Param("messages") List<MessageModel> messages);
 
     /**
      * 删除消息
      * 两种不同的入参，本质上都是通过消息id删除消息
      */
-    int deleteMessages(String queueName, List<MessageModel> messages);
-    int deleteMessagesById(String queueName, List<Integer> messageIds);
+    int deleteMessages(String queueName,     @Param("messages") List<MessageModel> messages);
+    int deleteMessagesById(String queueName, @Param("messageIds") List<Integer> messageIds);
 
     /**
      * 获取counts条消息
      * @param counts 消息的数目
      * @return 消息
      */
-    List<String> selectMessages(String queueName, int counts);
+    List<MessageModel> selectMessages(String queueName, @Param("counts") int counts);
 }
