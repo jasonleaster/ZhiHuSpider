5c8b68e61aef3044e94002a8ad27662be5ee4237
jasonleaster <jasonleaster@gmail.com>
2017-07-10 23:53:02 +0800

    添加消息模型

--- src/main/java/org/jasonleaster/spiderz/message/model/MessageModel.java
+++ src/main/java/org/jasonleaster/spiderz/message/model/MessageModel.java
@@ -1,30 +1,23 @@
 package org.jasonleaster.spiderz.message.model;
 
+import lombok.Data;
+
 /**
  * Author: jasonleaster
  * Date  : 2017/6/20
  * Email : jasonleaster@gmail.com
  * Description:
  */
+@Data
 public class MessageModel {
 
+    /**
+     * 消息id
+     */
     private long messageId;
 
+    /**
+     * 消息内容
+     */
     private String message;
-
-    public long getMessageId() {
-        return messageId;
-    }
-
-    public void setMessageId(long messageId) {
-        this.messageId = messageId;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
